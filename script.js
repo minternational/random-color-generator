8d78173b4eba9f0cf13d03da4ff372a1663e7f5e
minternational <mintkuehn@gmail.com>
2020-06-15 21:22:23 +0200

    fixed the issue just generate one color, added the other 4 colors as well

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 let newColors = [];
 
 function randomColor() {
@@ -9,9 +9,11 @@ function randomColor() {
     ].toString();
     newColors.push(randomPick);
   }
-  return `#${newColors.join("")}`;
+  return `#${newColors.join('')}`;
 }
 
 const handleClick = () => {
-  document.getElementById("color_1").style.backgroundColor = randomColor();
+  for (let i = 1; i < 6; ++i) {
+    document.getElementById(`color${i}`).style.backgroundColor = randomColor();
+  }
 };
