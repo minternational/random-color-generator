6a2ea06bfdb8812f25389159f36d1870944011d5
minternational <mintkuehn@gmail.com>
2020-06-15 16:10:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let newColors = [];
+
+function randomColor() {
+  newColors = [];
+  for (let i = 0; i < 6; i++) {
+    const randomPick = colors[
+      Math.floor(Math.random(colors[i]) * colors.length)
+    ].toString();
+    newColors.push(randomPick);
+  }
+  return `#${newColors.join("")}`;
+}
+
+const handleClick = () => {
+  document.getElementById("color_1").style.backgroundColor = randomColor();
+};
